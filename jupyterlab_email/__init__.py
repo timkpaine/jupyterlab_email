85b0bab1c4a9acd0c1811cb2ac8c4286c0c75fbc
Tim Paine <t.paine154@gmail.com>
2019-08-15 17:16:22 -0400

    dist changes

--- jupyterlab_email/__init__.py
+++ jupyterlab_email/__init__.py
@@ -2,11 +2,10 @@ from .nbconvert import run as run_nbconvert  # noqa: F401
 from ._email import email as email_notebook  # noqa: F401
 from .attachments import attach, latex  # noqa: F401
 
+from ._version import __version__  # noqa: F401
+
 
 def _jupyter_server_extension_paths():
     return [{
         "module": "jupyterlab_email.extension"
     }]
-
-
-__version__ = '0.2.0'
