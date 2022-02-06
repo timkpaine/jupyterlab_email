8699e7cb0ab5b47deb47242724966cc54760793f
Tim Paine <t.paine154@gmail.com>
2022-02-06 16:54:44 -0500

    move to prebuilt extension, bump deps

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ from jupyter_packaging import (
     install_npm,
     ensure_targets,
     combine_commands,
-    ensure_python,
     get_version,
 )
 
 pjoin = path.join
-
-ensure_python(("2.7", ">=3.3"))
 
 name = "jupyterlab_email"
 here = path.abspath(path.dirname(__file__))
@@ -27,7 +24,7 @@ requires = [
     "beautifulsoup4>=4.9.1",
     "emails>=0.5.15",
     "ipython>=7.2.0",
-    "jupyterlab>=1.0.0",
+    "jupyterlab>=3.0.0",
     "pandas>=0.23.4",
     "python-magic>=0.4.15",
 ]
@@ -48,7 +45,11 @@ dev_requires = requires + [
 
 data_spec = [
     # Lab extension installed by default:
-    ("share/jupyter/lab/extensions", "lab-dist", "jupyterlab_email-*.tgz"),
+    (
+        "share/jupyter/labextensions/jupyterlab_email",
+        "jupyterlab_email/labextension",
+        "**",
+    ),
     # Config to enable server extension by default:
     ("etc/jupyter/jupyter_server_config.d", "jupyter-config", "*.json"),
 ]
@@ -58,7 +59,11 @@ cmdclass = create_cmdclass("js", data_files_spec=data_spec)
 cmdclass["js"] = combine_commands(
     install_npm(jshere, build_cmd="build:all"),
     ensure_targets(
-        [pjoin(jshere, "lib", "index.js"), pjoin(jshere, "style", "index.css")]
+        [
+            pjoin(jshere, "lib", "index.js"),
+            pjoin(jshere, "style", "index.css"),
+            pjoin(here, "jupyterlab_email", "labextension", "package.json"),
+        ]
     ),
 )
 
@@ -75,13 +80,12 @@ setup(
     license="Apache 2.0",
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
+        "Framework :: Jupyter :: JupyterLab",
     ],
     cmdclass=cmdclass,
     keywords="jupyter jupyterlab",
@@ -96,4 +100,5 @@ setup(
     },
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.7",
 )
